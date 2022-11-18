c9160eca79295ef5214255cf1f585500d516238d
bcode <116942079+devbfactor@users.noreply.github.com>
2022-11-18 16:49:04 +0800

    commit

--- frontend/src/pages/Ecommerce.jsx
+++ frontend/src/pages/Ecommerce.jsx
@@ -10,8 +10,8 @@ const Ecommerce = () => {
 
   return (
     <div className="mt-12">
-      <div className="flex flex-wrap lg:flex-nowrap justify-center">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 welcom-bg bg-hero-pattern bg-cover bg-center">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center ">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-72 p-8 pt-9 my-3 ml-5 welcom-bg bg-hero-pattern bg-cover bg-center">
           <div className="flex justify-between items-center">
             <div>
               <p className="font-bold text-gray-400">Earnings</p>
@@ -25,7 +25,7 @@ const Ecommerce = () => {
 
         <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
           {earningData.map((item) => (
-            <div key={item.title} className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl">
+            <div key={item.title} className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl m-1">
               <button type="button" style={{ color: item.iconColor, backgroundColor: item.iconBg }} className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl">
                 {item.icon}
               </button>
